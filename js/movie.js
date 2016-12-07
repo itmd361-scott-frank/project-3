90e897a53bd92f4f47887d872500322da403a4af
ScottPartacz <Sparatcz@hawk.iit.edu>
2016-12-07 16:21:37 -0600

    fixed all of the errors with htmllint

--- js/movie.js
+++ js/movie.js
@@ -10,22 +10,22 @@ $(document).ready(function(){
           function(data,json) {
             var movie = data.Poster;
             if(movie !== null) {
-              $('#ERROR').empty();
-              $('#Rating').text('Rated: ' + data.Rated);
-              $('#Release').text('Release: ' + data.Released);
-              $('#Runtime').text('Runtime: ' + data.Runtime);
+              $('#error').empty();
+              $('#rating').text('Rated: ' + data.Rated);
+              $('#release').text('Release: ' + data.Released);
+              $('#runtime').text('Runtime: ' + data.Runtime);
               $('#poster').html('<a href="http://www.imdb.com/find?ref_=nv_sr_fn&q='+poster+'&s=all"><img id="thePoster" src=' + movie + ' alt = "Poster Not Found" /></a>');
-              $('#Plot').text('Plot: ' + data.Plot);
-              $('#Metascore').text('MetaScore: ' + data.Metascore);
+              $('#plot').text('Plot: ' + data.Plot);
+              $('#metascore').text('MetaScore: ' + data.Metascore);
               $('#imdb').html('<a href="http://www.imdb.com/title/' + data.imdbID + '/">IMDBRating: ' + data.imdbRating + '</a>');
             }
             else {
-              $('#ERROR').text('Error: ' + data.Error);
-              $('#Rating').empty();
-              $('#Release').empty();
-              $('#Runtime').empty();
-              $('#Plot').empty();
-              $('#Metascore').empty();
+              $('#error').text('Error: ' + data.Error);
+              $('#rating').empty();
+              $('#release').empty();
+              $('#runtime').empty();
+              $('#plot').empty();
+              $('#metascore').empty();
               $('#imdb').empty();
               $('#poster').html('<img id="thePoster" src="https://vignette2.wikia.nocookie.net/assassinscreed/images/3/39/Not-found.jpg/revision/latest?cb=20110517171552" alt = "No Photo found" />');
             }
